2eecfe170aee97bf4589d856b93700f13f57244e
dancey-apple <dancey.apple@proton.me>
2024-05-25 12:40:46 -0500

    a little work..

--- cup-o-java/src/components/Home.js
+++ cup-o-java/src/components/Home.js
@@ -1,13 +1,8 @@
 export default function Home() {
 
-    const [counter, setCounter] = usetState(0);
-
-    useEffect(() => {
-        console.log('counter', counter);
-}, [counter]);
-
 return (
     <>
         <div>Home</div>
     </>
 )
+}
---
+++ cup-o-java/src/components/NavBar.js
@@ -0,0 +1,14 @@
+export default function NavBar() {
+    return (
+        <>
+            <div className='headfootcontainer' id='navbar'>
+                <div>
+                    <h1>Cup O' Java</h1>
+                    <h1>Menu</h1>
+                    <h1>Locations</h1>
+                    <h1>Merchandise</h1>
+                </div>
+            </div>
+        </>
+    )
+}
